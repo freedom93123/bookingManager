3723539d85f6ae497b142d9e7382cb68f36dd00d
freedom93123 <freedom93123@gmail.com>
2016-02-15 04:18:25 +0800

    Second Commit Signup and login update

---
+++ RiderViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RiderViewController.swift
+//  ParseStarterProject-Swift
+//
+//  Created by FOEIT on 2/15/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import Cocoa
+
+class RiderViewController: UIViewController {
+
+}
